dd21c13b2128ce82482b28b15d8ab6487069999d
gps996 <gparkesskelly@gmail.com>
2017-10-20 10:10:22 +1100

    schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20171019225502) do
 
-  create_table "customers", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "login"
