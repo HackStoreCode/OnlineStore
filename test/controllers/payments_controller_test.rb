b0589ef6fb8d39a3d695eaa1fbc2ee3ec2e7fb06
Alex Pham <alex.pham2010@gmail.com>
2017-10-19 23:14:03 +1100

    Add scaffold for Payment

---
+++ test/controllers/payments_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class PaymentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @payment = payments(:one)
+  end
+
+  test "should get index" do
+    get payments_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_payment_url
+    assert_response :success
+  end
+
+  test "should create payment" do
+    assert_difference('Payment.count') do
+      post payments_url, params: { payment: { description: @payment.description, ordergroup_id: @payment.ordergroup_id, payment_date: @payment.payment_date, total_amount: @payment.total_amount } }
+    end
+
+    assert_redirected_to payment_url(Payment.last)
+  end
+
+  test "should show payment" do
+    get payment_url(@payment)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_payment_url(@payment)
+    assert_response :success
+  end
+
+  test "should update payment" do
+    patch payment_url(@payment), params: { payment: { description: @payment.description, ordergroup_id: @payment.ordergroup_id, payment_date: @payment.payment_date, total_amount: @payment.total_amount } }
+    assert_redirected_to payment_url(@payment)
+  end
+
+  test "should destroy payment" do
+    assert_difference('Payment.count', -1) do
+      delete payment_url(@payment)
+    end
+
+    assert_redirected_to payments_url
+  end
+end
