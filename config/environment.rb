93d9459d8544f8a6c8b07ba99760b61910bfc557
gps996 <gparkesskelly@gmail.com>
2017-10-20 09:20:29 +1100

    mailer issues

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'example.com',
+   user_name:            '<username>',
+   password:             '<password>',
+   authentication:       'plain',
+   enable_starttls_auto: true
+}
+
+ActionMailer::Base.default_content_type = "text/html"
